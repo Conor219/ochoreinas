2efc8c28a8c453fdca8b6cb6ca1196b81a5b8ac2
Conor <daniel-attwell@hotmail.com>
2025-02-01 09:34:04 -0700

    Funciones agregadas (cambiarSolucion, etc)

--- js/functions.js
+++ js/functions.js
@@ -55,3 +55,86 @@ function limpiarCelda(){
     var celdas = document.getElementsByTagName("td");
     for (let i = 0; i < celdas.length; i++) celdas[i].style.backgroundColor = "";
 }
+
+function cambiarSolucion() {
+    var solucion = document.getElementById("solucionSelect").value;
+    
+    // Limpiar el tablero antes de aplicar una nueva solución
+    limpiarTablero();
+
+    if (solucion === "solucion1") {
+        solucionUno();
+    } else if (solucion === "solucion2") {
+        solucionDos();
+    } else if (solucion === "solucion3") {
+        solucionTres();
+    }
+}
+
+function solucionUno(){
+    var estilo = `background-image:url(./img/reina.png);
+                  background-size:50px;
+                  background-repeat:no-repeat;   
+                  background-position:center;`;
+    
+    var celdas = document.getElementById("tablero");
+    celdas.rows[0].cells[3].style = estilo;
+    celdas.rows[1].cells[6].style = estilo;
+    celdas.rows[2].cells[2].style = estilo;
+    celdas.rows[3].cells[7].style = estilo;
+    celdas.rows[4].cells[1].style = estilo;
+    celdas.rows[5].cells[4].style = estilo;
+    celdas.rows[6].cells[0].style = estilo;
+    celdas.rows[7].cells[5].style = estilo;
+}
+
+function solucionDos(){
+    var estilo = `background-image:url(./img/reina.png);
+                  background-size:50px;
+                  background-repeat:no-repeat;   
+                  background-position:center;`;
+    
+    var celdas = document.getElementById("tablero");
+    celdas.rows[0].cells[4].style = estilo;
+    celdas.rows[1].cells[1].style = estilo;
+    celdas.rows[2].cells[3].style = estilo;
+    celdas.rows[3].cells[6].style = estilo;
+    celdas.rows[4].cells[2].style = estilo;
+    celdas.rows[5].cells[7].style = estilo;
+    celdas.rows[6].cells[5].style = estilo;
+    celdas.rows[7].cells[0].style = estilo;
+}
+
+function solucionTres(){
+    var estilo = `background-image:url(./img/reina.png);
+                  background-size:50px;
+                  background-repeat:no-repeat;   
+                  background-position:center;`;
+    
+    var celdas = document.getElementById("tablero");
+    celdas.rows[0].cells[3].style = estilo;
+    celdas.rows[1].cells[1].style = estilo;
+    celdas.rows[2].cells[6].style = estilo;
+    celdas.rows[3].cells[2].style = estilo;
+    celdas.rows[4].cells[5].style = estilo;
+    celdas.rows[5].cells[7].style = estilo;
+    celdas.rows[6].cells[4].style = estilo;
+    celdas.rows[7].cells[0].style = estilo;
+}
+
+function reiniciarTablero() {
+    // Limpiar todos los estilos del tablero
+    limpiarTablero();
+    
+    // Restablecer el select a su valor predeterminado (---)
+    document.getElementById("solucionSelect").value = "default";
+}
+
+function limpiarTablero() {
+    var celdas = document.getElementById("tablero");
+    for (var i = 0; i < celdas.rows.length; i++) {
+        for (var j = 0; j < celdas.rows[i].cells.length; j++) {
+            celdas.rows[i].cells[j].style = ""; // Limpiar los estilos
+        }
+    }
+}
